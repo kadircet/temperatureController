1a9cbe4e9103a2004f5e19bef7918518fd249d1d
Kadir Cetinkaya <kadircet@google.com>
2022-10-31 13:30:10 +0100

    make it compile

--- temperatureController/test/temperature_controller_test.cc
+++ temperatureController/test/temperature_controller_test.cc
@@ -1,35 +1,35 @@
-#include "gtest/gtest.h"
-#include "mock_fan_speed.h"
-#include "mock_temperature_sensor.h"
 #include "../src/machine.h"
 #include "../src/temperature_controller.h"
+#include "mock_fan_speed.h"
+#include "mock_temperature_sensor.h"
+#include "gtest/gtest.h"
 
 /*	Status Control & Fan Speed Control
-*	Since sensor implementation does not exist
-*	Fixed room temperature are read from sensor as 25
-*	Desired temperature changes
-*	Random tests are conducted.
-*/
-TEST(Air_Conditioner_Test, WhenSetTemperatureEqualsRoomTemperature_ThenReturnSystemOff) {
-	
-    unique_ptr<TemperatureSensor> tempSensor = make_unique<TemperatureSensor>();
-    MockTemperatureSensor mockSensor{move(tempSensor)};
-	
-    unique_ptr<FanSpeed> fanSpeed = make_unique<FanSpeed>();
-    MockFanSpeed mockFan{move(fanSpeed)};
-	
-	TemperatureController homeTempController = TemperatureController(move(mockFan), move(mockSensor));
+ *	Since sensor implementation does not exist
+ *	Fixed room temperature are read from sensor as 25
+ *	Desired temperature changes
+ *	Random tests are conducted.
+ */
+TEST(Air_Conditioner_Test,
+     WhenSetTemperatureEqualsRoomTemperature_ThenReturnSystemOff) {
 
-	eFanSpeedSettings readFanSpeed = eFanSpeedSettings::eFAN_SPEED_OFF;
-	eCurrentStatus currentStatus = eCurrentStatus::SYSTEM_OFF;
-	
-	//Check normal conditions
-	homeTempController.setSetTemperature(25);
-	homeTempController.mainController();
-	
-	readFanSpeed = fanSpeed->getFanSpeed();
-	EXPECT_EQ(eFanSpeedSettings::eFAN_SPEED_OFF, readFanSpeed);
-	
-	currentStatus = homeTempController.getCurrentStatus();
-	EXPECT_EQ(eCurrentStatus::SYSTEM_OFF, currentStatus);
+  unique_ptr<MockTemperatureSensor> mockSensor =
+      make_unique<MockTemperatureSensor>();
+  unique_ptr<MockFanSpeed> mockFan = make_unique<MockFanSpeed>();
+  auto fanptr = mockFan.get();
+
+  TemperatureController homeTempController(move(mockFan), move(mockSensor));
+
+  eFanSpeedSettings readFanSpeed = eFanSpeedSettings::eFAN_SPEED_OFF;
+  eCurrentStatus currentStatus = eCurrentStatus::SYSTEM_OFF;
+
+  // Check normal conditions
+  homeTempController.setSetTemperature(25);
+  homeTempController.mainController();
+
+  readFanSpeed = fanptr->getFanSpeed();
+  EXPECT_EQ(eFanSpeedSettings::eFAN_SPEED_OFF, readFanSpeed);
+
+  currentStatus = homeTempController.getCurrentStatus();
+  EXPECT_EQ(eCurrentStatus::SYSTEM_OFF, currentStatus);
 }
